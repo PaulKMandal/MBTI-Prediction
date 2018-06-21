002dd0115c1ab7b36dd383047ef572b3620e7849
rat395 <rat395@ymail.com>
2018-06-21 12:05:21 -0700

    got rid of unnecessary comments

--- csv_to_json.py
+++ csv_to_json.py
@@ -12,18 +12,3 @@ reader = csv.DictReader(csvDataFile, fieldNames)
 for row in reader:
         json.dump(row, jsonDataFile)
         jsonDataFile.write('\n')
-
-# saving each line of the data(type, post) into a list for formatting
-# dataList = list(dataFile)
-
-# print(dataList[1][7:len(dataList[1]) - 3])
-# data = {}
-# data['type'] = dataList[1][:4]
-# data['post'] = dataList[1][7:len(dataList[1]) - 3]
-# data['type'] = dataList[2][:4]
-# data['post'] = dataList[2][7:len(dataList[1]) - 3]
-# json_data = json.dumps(data)
-
-# with open('formatted_data.json', 'w') as outfile:
-#    json.dump(json_data, outfile)
-
